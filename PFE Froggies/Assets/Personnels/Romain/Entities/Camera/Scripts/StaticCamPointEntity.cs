b8a826cec7af9f89edaec1ade047d486125be657
Ashobot <42091725+Ashobot@users.noreply.github.com>
2023-11-07 15:07:33 +0100

    Squashed commit of the following:
    commit b7710ef3bd028cbd8ebbdaf5453f55e35419a365
    Author: Ashobot <42091725+Ashobot@users.noreply.github.com>
    Date:   Tue Nov 7 15:05:26 2023 +0100
    
        Caméra fonctionelle =) (je suis trop fier walla)
    
    commit 1937666ca8b198c13e35f1ccf833baa9115bbc3d
    Author: Ashobot <42091725+Ashobot@users.noreply.github.com>
    Date:   Thu Oct 26 12:02:26 2023 +0200
    
        Camera puzzle
    
    commit a1847fa0fc66fd64f5a39947b06a5187cb84af4a
    Author: Ashobot <42091725+Ashobot@users.noreply.github.com>
    Date:   Mon Oct 23 15:58:15 2023 +0200
    
        Caméra
    
    commit ad430ba445b4f73d66f4eba7424a1d62f5af9280
    Author: Ashobot <42091725+Ashobot@users.noreply.github.com>
    Date:   Mon Oct 23 15:15:18 2023 +0200
    
        Squashed commit of the following:
    
        commit 9ece13f52415912117edbfca3593aef8e2eeba51
        Author: Fostep <tristan92.capony@gmail.com>
        Date:   Thu Oct 19 15:45:17 2023 +0200
    
            Correction grounded infinite jump
    
    commit 7dbd0da6a1946005323643c43db02f09bb33d194
    Author: Ashobot <42091725+Ashobot@users.noreply.github.com>
    Date:   Mon Oct 23 15:11:42 2023 +0200
    
        Debug de la caméra quand on spawn
    
    commit cdb23282cc0e2f1be739aa23f9e6fcc7b3818787
    Author: Ashobot <42091725+Ashobot@users.noreply.github.com>
    Date:   Thu Oct 19 15:55:23 2023 +0200
    
        CAMERA DE CON DE TA MERE LA ****
    
        =) je veux mourrir (vraiment, aidez moi...)

---
+++ PFE Froggies/Assets/Personnels/Romain/Entities/Camera/Scripts/StaticCamPointEntity.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticCamPointEntity : MonoBehaviour
+{
+    [SerializeField] CameraEntity camEntity;
+    [SerializeField] Transform camPositionTransform;
+    [Space]
+    [SerializeField] List<GameObject> playersInZone = new List<GameObject>(2);
+    [SerializeField] LayerMask playersLayerMask;
+
+    private void Awake()
+    {
+        camEntity = Camera.main.GetComponent<CameraEntity>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if((playersLayerMask.value & (1 << other.transform.gameObject.layer)) > 0 && !playersInZone.Contains(other.gameObject))
+        {
+            playersInZone.Add(other.gameObject);
+
+            if (playersInZone.Count == 2)
+            {
+                camEntity.SetNewCamTargetPoint(camPositionTransform);
+            }
+        }     
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ((playersLayerMask.value & (1 << other.transform.gameObject.layer)) > 0 && playersInZone.Contains(other.gameObject))
+        {
+            playersInZone.Remove(other.gameObject);
+
+            if (playersInZone.Count < 2)
+            {
+                camEntity.SetExplorationCam();
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, GetComponent<BoxCollider>().size);
+    }
+}
