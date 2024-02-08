19cac17ba657ed3382990cfb08e3df51f4cdd359
Fostep <tristan92.capony@gmail.com>
2024-02-08 16:17:46 +0100

    RocMoving

--- PFE Froggies/Assets/Objects/Interactable/Scripts/InteractableRockEntity.cs
+++ PFE Froggies/Assets/Objects/Interactable/Scripts/InteractableRockEntity.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableDuoEntity : InteractableEntity, IInteractableEntity
+public class InteractableRockEntity : InteractableEntity, IInteractableEntity
 {
     [Header("Time to hit Parameters")]
     [SerializeField] public float timeTriedToBePushed = 1f;
@@ -10,15 +10,16 @@ public class InteractableDuoEntity : InteractableEntity, IInteractableEntity
     [SerializeField] bool _isTriedToBePushed = false;
     [SerializeField] GameObject _frogFirstHit;
 
-
+    [Header("Moving Parameters")]
+    [SerializeField] public float distanceToMove = 1;
 
     private void FixedUpdate()
     {
         // A player tried to push it
-        if(_isTriedToBePushed)
+        if (_isTriedToBePushed)
         {
             // Timer waiting for other player to hit it
-            if(_timeTried < timeTriedToBePushed)
+            if (_timeTried < timeTriedToBePushed)
                 _timeTried += Time.fixedDeltaTime;
             else
             {
@@ -33,7 +34,26 @@ public class InteractableDuoEntity : InteractableEntity, IInteractableEntity
     {
         // Has already been hit
         if (_isTriedToBePushed && _frogFirstHit != frog)
-            base.Push(dir, force, frog);
+        {
+            // base.Push(dir, force, frog);
+
+            // TAKE ONLY 1 AXIS
+            Vector3 direction = (this.transform.position - frog.transform.position);
+            direction.y = 0;
+
+            if(direction.x > direction.z)
+            {
+                direction.x = 1;
+                direction.z = 0;
+            }
+            else
+            {
+                direction.x = 0;
+                direction.z = 1;
+            }
+            _rb.AddForce(direction * force, ForceMode.VelocityChange);
+
+        }
         // 1st time hit
         else
         {
