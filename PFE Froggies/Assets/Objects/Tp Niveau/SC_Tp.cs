3c05422af8c183ce54c891c24cbd790c5a81b3b8
Fostep <73890826+Fostep@users.noreply.github.com>
2024-02-10 00:55:45 +0100

    Correction Loading Scene
    Parce que je suis le seul a taffer

--- PFE Froggies/Assets/Objects/Tp Niveau/SC_Tp.cs
+++ PFE Froggies/Assets/Objects/Tp Niveau/SC_Tp.cs
@@ -7,17 +7,31 @@ public class SC_Tp : MonoBehaviour {
 
     float Nbplayer = 0;
 
-    public void OnTriggerEnter(Collider other) {
+    public void OnTriggerEnter(Collider other) 
+    {
+        if(other.transform.TryGetComponent<PlayerEntity>(out PlayerEntity player))
+        {
+            Nbplayer += 1;
+        }
 
-        Nbplayer += 1;
-
-        if (Nbplayer >= 2 && SceneManager.GetActiveScene().name == "N_Tuto") {
-
-            SceneManager.LoadScene(1);
-
-        } else {
+        
+        if (Nbplayer >= 2 && SceneManager.GetActiveScene().buildIndex >= SceneManager.sceneCount-1) {
 
             SceneManager.LoadScene(0);
+
+        } 
+        else 
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.TryGetComponent<PlayerEntity>(out PlayerEntity player))
+        {
+            Nbplayer -= 1;
         }
     }
 }
