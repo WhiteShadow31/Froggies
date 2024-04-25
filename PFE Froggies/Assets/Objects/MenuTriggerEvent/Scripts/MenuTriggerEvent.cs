f85dd35421fed10a067b997f03f4ab29c57f2f20
Ashobot <42091725+Ashobot@users.noreply.github.com>
2024-04-25 13:47:26 +0200

    Boutons saves, play et retour du menu

--- PFE Froggies/Assets/Objects/MenuTriggerEvent/Scripts/MenuTriggerEvent.cs
+++ PFE Froggies/Assets/Objects/MenuTriggerEvent/Scripts/MenuTriggerEvent.cs
@@ -4,34 +4,44 @@ using UltimateAttributesPack;
 
 public class MenuTriggerEvent : MonoBehaviour
 {
+    [SerializeField] GameObject _transitionObject;
+
     [LineTitle("Event")]
-    [SerializeField] int _triggerIfPlayersInCount;
-    [SerializeField] float _triggerAfter;
+    [SerializeField] int _activateIfPlayersCountInTrigger;
+    [SerializeField] float _activationTime;
+    [SerializeField] Vector3 _activationTargetScale;
+    [SerializeField] AnimationCurve _activationCurve;
     [SerializeField] LayerMask _playersLayerMask;
+    [Space]
+    [SerializeField] GameObject _textObject;
     [SerializeField] GameObject[] _objectsToActivate;
     [SerializeField] GameObject[] _objectsToDesactivate;
     [SerializeField] UnityEvent _event;
+    [Space]
+    [SerializeField] Vector3 _baseScale;
+    [SerializeField] Color _baseColor;
 
     int _playersInTriggerCount = 0;
-    float _triggerTimer;
+    float _activationTimer;
 
     [LineTitle("Transition")]
-    [SerializeField] GameObject _transitionObject;
-    [SerializeField] Vector3 _transitionTargetScale;
+    [SerializeField] bool _hasTransition;
+    [SerializeField, ShowIf(nameof(_hasTransition), true)] Vector3 _transitionTargetScale;
     [Space]
-    [SerializeField] float _transitionOutTime;
-    [SerializeField] Color _trasitionOutColor;
-    [SerializeField] AnimationCurve _transitionOutCurve;
+    [SerializeField, ShowIf(nameof(_hasTransition), true)] float _transitionOutTime;
+    [SerializeField, ShowIf(nameof(_hasTransition), true)] Color _trasitionOutColor;
+    [SerializeField, ShowIf(nameof(_hasTransition), true)] AnimationCurve _transitionOutCurve;
     [Space]
-    [SerializeField] float _transitionInTime;
-    [SerializeField] Color _trasitionInColor;
-    [SerializeField] AnimationCurve _transitionInCurve;
+    [SerializeField, ShowIf(nameof(_hasTransition), true)] float _transitionInTime;
+    [SerializeField, ShowIf(nameof(_hasTransition), true)] Color _trasitionInColor;
+    [SerializeField, ShowIf(nameof(_hasTransition), true)] AnimationCurve _transitionInCurve;
 
-    Color _baseColor;
-    Vector3 _baseScale;
     bool _inTransition;
     bool _isTransitionIn;
     float _transitionTimer;
+
+    [Title("Debug")]
+    [SerializeField] bool _showDebug;
 
     MeshRenderer _meshRenderer;
 
@@ -40,51 +50,59 @@ public class MenuTriggerEvent : MonoBehaviour
         _meshRenderer = _transitionObject.GetComponent<MeshRenderer>();
     }
 
-    private void Start()
-    {
-        _baseScale = _transitionObject.transform.localScale;
-        _baseColor = _meshRenderer.sharedMaterial.color;
-    }
-
     private void Update()
     {
-        if(_playersInTriggerCount == _triggerIfPlayersInCount || _inTransition)
+        // Activation
+        if (!_inTransition)
         {
-            if (_triggerTimer < _triggerAfter)
+            if(_playersInTriggerCount == 2)
             {
-                _triggerTimer += Time.deltaTime;
+                if(_activationTimer < _activationTime)
+                    _activationTimer = Mathf.Clamp(_activationTimer += Time.deltaTime, 0, _activationTime);
+                else
+                    _inTransition = true;
             }
-            else if(!_isTransitionIn)
+            else
+                _activationTimer = Mathf.Clamp(_activationTimer -= Time.deltaTime, 0, _activationTime);
+
+            // Lerp scale
+            _transitionObject.transform.localScale = Vector3.Lerp(_baseScale, _activationTargetScale, _activationCurve.Evaluate(_activationTimer / _activationTime));
+        }
+        // Transition Out
+        else if (_inTransition && _hasTransition)
+        {
+            if (!_isTransitionIn)
             {
-                _inTransition = true;
-                
                 // Transition out
                 if(_transitionTimer < _transitionOutTime)
                 {
-                    _transitionObject.transform.localScale = Vector3.Lerp(_baseScale, _transitionTargetScale, _transitionOutCurve.Evaluate(_transitionTimer / _transitionOutTime));
+                    // Lerp scale and color
+                    _transitionObject.transform.localScale = Vector3.Lerp(_activationTargetScale, _transitionTargetScale, _transitionOutCurve.Evaluate(_transitionTimer / _transitionOutTime));
                     _meshRenderer.sharedMaterial.color = Color.Lerp(_baseColor, _trasitionOutColor, _transitionOutCurve.Evaluate(_transitionTimer / _transitionOutTime));
 
                     _transitionTimer += Time.deltaTime;
                 }
                 else
                 {
+                    // Set scale and color to target
                     _transitionObject.transform.localScale = _transitionTargetScale;
                     _meshRenderer.sharedMaterial.color = _trasitionOutColor;
 
                     // Play events
-                    ActivateTargetObjects();
-                    DesactivateTargetObjects();
-                    _event.Invoke();
+                    PlayEvents();
 
+                    // Activate transition in
                     _isTransitionIn = true;
                     _transitionTimer = 0f;
                 }               
             }
+            // Transition In
             else
             {
                 // Transition in
                 if (_transitionTimer < _transitionOutTime)
                 {
+                    // Lerp scale and color
                     _transitionObject.transform.localScale = Vector3.Lerp(_transitionTargetScale, Vector3.zero, _transitionOutCurve.Evaluate(_transitionTimer / _transitionOutTime));
                     _meshRenderer.sharedMaterial.color = Color.Lerp(_trasitionOutColor, _trasitionInColor, _transitionOutCurve.Evaluate(_transitionTimer / _transitionOutTime));
 
@@ -92,6 +110,7 @@ public class MenuTriggerEvent : MonoBehaviour
                 }
                 else
                 {
+                    // Set scale and color to target
                     _transitionObject.transform.localScale = Vector3.zero;
                     _meshRenderer.sharedMaterial.color = _trasitionInColor;
 
@@ -100,27 +119,69 @@ public class MenuTriggerEvent : MonoBehaviour
                 }
             }
         }
-    }
-
-    void ActivateTargetObjects()
-    {
-        foreach(GameObject go in _objectsToActivate)
+        // If there is no transition, then play events directly
+        else
         {
-            go.SetActive(true);
+            PlayEvents(); // Play events
+            DesactivateObject(); // Desactivate object
         }
     }
 
-    void DesactivateTargetObjects()
+    void PlayEvents()
     {
+        // Activate all objects to activate
+        foreach (GameObject go in _objectsToActivate)
+        {
+            go.SetActive(true);
+            if(go.TryGetComponent<MenuTriggerEvent>(out MenuTriggerEvent script))
+            {
+                script.ResetObject();
+            }
+        }
+
+        // Desactivate all objects to desactivate
         foreach (GameObject go in _objectsToDesactivate)
         {
-            go.SetActive(true);
+            go.SetActive(false);
         }
+
+        _textObject.SetActive(false); // Disable text
+        _event.Invoke(); // Play events
     }
 
     void DesactivateObject()
     {
         gameObject.SetActive(false);
+    }
+
+    public void ResetObject()
+    {
+        _transitionObject.transform.localScale = _baseScale;
+        _meshRenderer.sharedMaterial.color = _baseColor;
+        _textObject.SetActive(true);
+
+        _playersInTriggerCount = 0;
+        _activationTimer = 0;
+        _transitionTimer = 0;
+        _inTransition = false;
+        _isTransitionIn = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_showDebug)
+        {
+            // Draw activation circle
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(_transitionObject.transform.position, _activationTargetScale.x / 2);
+
+            if (_hasTransition)
+            {
+                // Draw transition circle
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(_transitionObject.transform.position, _transitionTargetScale.x / 2);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -129,7 +190,7 @@ public class MenuTriggerEvent : MonoBehaviour
         {
             _playersInTriggerCount++;
 
-            _triggerTimer = 0;
+            _activationTimer = 0;
             _transitionTimer = 0f;
         }
     }
