07efbb6128046e6207907e6081a808028968266d
Fostep <tristan92.capony@gmail.com>
2024-06-06 09:47:19 +0200

    Audio

--- PFE Froggies/Assets/Audio/AudioEntity.cs
+++ PFE Froggies/Assets/Audio/AudioEntity.cs
@@ -8,6 +8,7 @@ public class AudioEntity : MonoBehaviour
     GameObject _trackedObject;
     bool _isTracking = false;
 
+    bool _isLooping = false;
     float _length = 0;
     float _time = 0;
     bool _isPlaying = false;
@@ -20,22 +21,21 @@ public class AudioEntity : MonoBehaviour
 
     private void Update()
     {
-        if (_isPlaying)
+        if (!_isLooping && _time > _length)
         {
-            if(_time > _length )
-            {
-                Destroy(this.gameObject);
-            }
-            _time += Time.deltaTime;
+            Destroy(this.gameObject);
         }
+        _time += Time.deltaTime;
 
         if (_isTracking)
             this.transform.position = _trackedObject.transform.position;
     }
 
-    public void PlayClip(AudioClip clip)
+    public void PlayClip(AudioClip clip, bool isLooping = false)
     {
         _source.clip = clip;
+        _source.Play();
+        _isLooping = isLooping;
     }
 
     public void Track(GameObject go)
