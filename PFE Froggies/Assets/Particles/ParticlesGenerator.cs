d4fa30f2ddcbbcb4cb4dce8a94d333635fa89c81
Fostep <73890826+Fostep@users.noreply.github.com>
2024-06-13 15:45:14 +0200

    Color by tag Particles

--- PFE Froggies/Assets/Particles/ParticlesGenerator.cs
+++ PFE Froggies/Assets/Particles/ParticlesGenerator.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class ParticlesGenerator : MonoBehaviour
 {
+    [Header("Particles color by tag")]
+    public string tag1 = "Grass";
+    public List<Color> grassColor = new List<Color>(2);
+    public string tag2 = "Dirt";
+    public List<Color> dirtColor = new List<Color>(2);
+    public string tag3 = "Water";
+    public List<Color> waterColor = new List<Color>(2);
+    public string tag4 = "Rock";
+    public List<Color> rockColor = new List<Color>(2);
+    public string tag5 = "Wood";
+    public List<Color> woodColor = new List<Color>(2);
+
     [Header("Player Particles")]
     public GameObject touchGroundParticles;
     public GameObject highTouchGroundParticles;
     public GameObject jumpGroundParticles;
-    public GameObject salivateParticles;
+    public GameObject salivateInsectParticles;
+    public GameObject slideGround;
 
     [Header("Pollen Particles")]
     public GameObject pollenParticles;
@@ -21,40 +34,90 @@ public class ParticlesGenerator : MonoBehaviour
         _instance = this;
     }
 
-    public void PlayTouchGround(Vector3 position)
+    void SetParticlesEntityColor(GameObject particles, string tag)
+    {
+        ParticlesEntity pe = particles.GetComponent<ParticlesEntity>();
+        ParticleSystem.MainModule main = pe.Particles.main;
+        main.startColor = new ParticleSystem.MinMaxGradient(ColorByTag(tag)[0], ColorByTag(tag)[1]);;
+    }
+
+    List<Color> ColorByTag(string useTag)
+    {
+        if(useTag == tag1)
+            return grassColor;
+        else if(useTag == tag2)
+            return dirtColor;
+        else if(useTag == tag3)
+            return waterColor;
+        else if(useTag == tag4)
+            return rockColor;
+        else if(useTag == tag5)
+            return woodColor;
+        else
+            return new List<Color>(2){Color.magenta, Color.red};
+    }
+
+    public void PlayTouchGround(Vector3 position, string tag = "None")
     {
         if (touchGroundParticles != null)
         {
             GameObject particles = Instantiate(touchGroundParticles);
             particles.transform.position = position;
+
+            SetParticlesEntityColor(particles, tag);
         }
     }
 
-    public void PlayHighTouchGround(Vector3 position)
+    public void PlayHighTouchGround(Vector3 position, string tag = "None")
     {
         if (highTouchGroundParticles != null)
         {
             GameObject particles = Instantiate(highTouchGroundParticles);
             particles.transform.position = position;
+
+            SetParticlesEntityColor(particles, tag);
         }
     }
 
-    public void PlayJumpGround(Vector3 position, Vector3 jumpDirection)
+    public void PlayJumpGround(Vector3 position, Vector3 jumpDirection, string tag = "None")
     {
         if (jumpGroundParticles != null)
         {
             GameObject particles = Instantiate(jumpGroundParticles);
             particles.transform.position = position;
             particles.transform.rotation = Quaternion.LookRotation(jumpDirection);
+
+            SetParticlesEntityColor(particles, tag);
         }
     }
 
+    public void PlaySlideGround(GameObject player, string tag = "None")
+    {
+        if(slideGround != null)
+        {
+            GameObject particles = Instantiate(slideGround);
+            ParticlesEntity pe = particles.GetComponent<ParticlesEntity>();
+            pe.Follow(player);
+
+            SetParticlesEntityColor(particles, tag);
+        }
+    }
     public void PlayPollenSuccess(Vector3 position)
     {
         if (pollenParticles != null)
         {
             GameObject particles = Instantiate(pollenParticles);
             particles.transform.position = position;
+        }
+    }
+
+    public void PlaySalivateInsect(GameObject insect)
+    {
+        if(salivateInsectParticles != null)
+        {
+            GameObject particles = Instantiate(salivateInsectParticles);
+            ParticlesEntity pe = particles.GetComponent<ParticlesEntity>();
+            pe.Follow(insect);
         }
     }
 }
